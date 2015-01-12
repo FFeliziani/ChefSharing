f473294b26ab57157fbd5878ecac0e73d20d0e55
Federico Feliziani <me@federicofeliziani.com>
2015-01-12 01:11:47 +0100

    First Commit

---
+++ NewChefSharing/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Security;
+using System.Guid;
+
+namespace NewChefSharing.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public string Login(string username, string md5)
+        {
+            return "[INSERT HERE SESSION TOKEN!]";
+        }
+    }
+}
